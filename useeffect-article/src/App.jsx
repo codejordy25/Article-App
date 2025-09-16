95c7aae383d8db11857a250c88dc6937c1ef25c1
codejordy25 <jordytsimba11@gmail.com>
2025-09-16 17:13:58 +0200

    rendu condit et button H/S

--- useeffect-article/src/App.jsx
+++ useeffect-article/src/App.jsx
@@ -1,22 +1,33 @@
+import { useState } from "react";
 import Article from "./Article";
 
 function App({ title }) {
   title = "Comprendre UseEffect";
+  const [isvisible, setIsVisible] = useState(true);
+
+  function handleClick() {
+    setIsVisible((pre) => !pre); //Button pour faire varié la valeur de la propriété
+  }
+
   return (
     <div>
       <h2>{title}</h2>
-      <button>Hide Article</button>
-      <Article>
-        <h3>Hello World Article</h3>
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim,
-          tempore nisi! <br /> Possimus natus numquam provident nostrum dolores,{" "}
-          <br />
-          eius quod consectetur obcaecati dicta voluptatem earum voluptas id{" "}
-          <br />
-          assumenda voluptatibus sit nemo.
-        </p>
-      </Article>
+      <button onClick={handleClick}>
+        {isvisible ? "Hide Article" : "Show Article"}
+      </button>
+      {isvisible && (
+        <Article>
+          <h3>Hello World Article</h3>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim,
+            tempore nisi! <br /> Possimus natus numquam provident nostrum
+            dolores, <br />
+            eius quod consectetur obcaecati dicta voluptatem earum voluptas id{" "}
+            <br />
+            assumenda voluptatibus sit nemo.
+          </p>
+        </Article>
+      )}
     </div>
   );
 }
